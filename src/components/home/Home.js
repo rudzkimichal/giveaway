6ef1b5891f1c7a9de37e60a04a27a711156a5b55
rudzkimichal <michal.rudzki@yahoo.com>
2020-05-15 14:26:36 +0200

    added about us part

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './header/Header';
 import Stats from './Stats';
 import Steps from './Steps';
+import AboutUs from './AboutUs';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Header />
       <Stats />
       <Steps />
+      <AboutUs />
     </>
   );
 }
