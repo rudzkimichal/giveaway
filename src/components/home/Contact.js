2031876a4167f7460df97934062ba5d1ccc76736
rudzkimichal <michal.rudzki@yahoo.com>
2020-05-20 16:19:22 +0200

    added form validation and post method in contact part

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -6,6 +6,20 @@ import instagram from '../../assets/Instagram.svg';
 import facebook from '../../assets/Facebook.svg';
 
 const Contact = () => {
+
+  const sendMessage = async (url, data) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    console.log(data);
+
+    return response.json();
+  }
+
   return (
     <>
     <div id='contact'>
@@ -13,18 +27,53 @@ const Contact = () => {
       <div className='contact-form-container'>
         <h2>Skontaktuj się z nami</h2>
         <Divider />
-        <Formik>
+        <Formik
+          initialValues={{name: '', email: '', message: ''}}
+          validate = {values => {
+            const errors = {};
+            const fieldRequired = 'To pole jest wymagane';
+
+            if(!values.name)
+              errors.name = fieldRequired;
+            else if (values.name.includes(' '))
+              errors.name = 'Imię powinno być jednym wyrazem bez spacji';
+
+            if(!values.email)
+              errors.email = fieldRequired;
+            else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email))
+              errors.email = 'Nieprawidłowy adres';
+
+            if(!values.message)
+              errors.message = fieldRequired;
+            else if(values.message.length < 120)
+              errors.message = 'Wiadomość powinna zawierać co najmniej 120 znaków';
+
+            return errors;
+          }}
+          onSubmit = {(values, {setSubmitting}) => {
+            sendMessage(
+              'https://fer-api.coderslab.pl/v1/portfolio/contact',
+              {
+                "name": values.name,
+                "email": values.email,
+                "message": values.message
+              }
+            );
+            setSubmitting(false);
+          }}
+        >
+          {({isSubmitting}) => (
           <Form className='form'>
             <section>
               <div>
                 <label htmlFor='name'>Wpisz swoje imię</label>
                 <Field type='text' name='name' className='input' placeholder='Krzysztof' />
-                <ErrorMessage name='name' component='p' />
+                <ErrorMessage className='error-message' name='name' component='p' />
               </div>
               <div>
                 <label htmlFor='email'>Wpisz swój email</label>
                 <Field type='email' name='email' className='input' placeholder='abc@xyz.pl' />
-                <ErrorMessage name='email' component='p' />
+                <ErrorMessage className='error-message' name='email' component='p' />
               </div>
             </section>
             <div>
@@ -39,10 +88,11 @@ const Contact = () => {
                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                 laboris nisi ut aliquip ex ea commodo consequat.'
                 />
-              <ErrorMessage name='message' component='p' />
+              <ErrorMessage className='error-message' name='message' component='p' />
             </div>
-            <button type='submit'>Wyślij</button>
+            <button type='submit' disabled={isSubmitting}>Wyślij</button>
           </Form>
+        )}
         </Formik>
       </div>
     </div>
